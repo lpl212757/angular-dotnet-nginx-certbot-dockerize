a41b66b17f237a096bb2685c94d9b8b2f2ceb7e8
Loc Le <lpl212757@gmail.com>
2023-09-17 16:19:29 +0000

    MOD - Client call server

--- src/client/src/app/app.component.ts
+++ src/client/src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'client';
+  data: any = {};
+
+  constructor(private httpClient: HttpClient) {}
+
+  ngOnInit(): void {
+    const url = '/api/weatherForecast';
+    this.httpClient.get(url).subscribe(res => this.data = res);
+  }
+
 }
